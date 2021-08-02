089de67fc6791b583c4b308a0fc920dd6dc84730
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-08-03 01:47:10 +1000

    Got a basic random walk algo working + Misc scene updates

--- Assets/Script/RoadGenerator.cs
+++ Assets/Script/RoadGenerator.cs
@@ -4,17 +4,90 @@ using UnityEngine;
 
 public class RoadGenerator : MonoBehaviour
 {
-    public GameObject[] houses;
+    public GameObject[] housePrefabs;
+    public GameObject road;
 
-    void Start()
+    private void Start()
+    {
+        RandomWalk(0, 0, 1000);
+    }
+
+    private void RandomWalk(int globalX, int globalZ, int counter)
+    {
+        if (counter > 0)
+        {
+            counter--;
+            int length = 3;
+
+            Instantiate(road, new Vector3(globalX, 0, globalZ), Quaternion.identity);
+
+            int randDirection = Random.Range(0, 4);
+            switch (randDirection)
+            {
+                case 0:
+                    {
+                        for (int z = 0; z < length; z++)
+                        {
+                            Instantiate(road, new Vector3(globalX, 0, globalZ + z), Quaternion.identity);
+                        }
+                        RandomWalk(globalX, globalZ + length, counter);
+                        break;
+                    }
+
+                case 1:
+                    {
+                        for (int z = -length; z < 0; z++)
+                        {
+                            Instantiate(road, new Vector3(globalX, 0, globalZ + z), Quaternion.identity);
+                        }
+                        RandomWalk(globalX, globalZ - length, counter);
+                        break;
+                    }
+
+                case 2:
+                    {
+                        for (int x = 0; x < length; x++)
+                        {
+                            Instantiate(road, new Vector3(globalX + x, 0, globalZ), Quaternion.identity);
+                        }
+                        RandomWalk(globalX + length, globalZ, counter);
+                        break;
+                    }
+
+                case 3:
+                    {
+                        for (int x = -length; x < 0; x++)
+                        {
+                            Instantiate(road, new Vector3(globalX + x, 0, globalZ), Quaternion.identity);
+                        }
+                        RandomWalk(globalX - length, globalZ, counter);
+                        break;
+                    }
+            }
+
+        }
+    }
+
+    private void GenerateRoads()
+    {
+        for (float x = -9.5f; x < 10; x++)
+        {
+            for (float z = -9.5f; z < 10; z++)
+            {
+                Instantiate(road, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+
+    private void GenerateHouses()
     {
         for (int x = -9; x < 10; x += 3)
         {
             for (int z = -9; z < 10; z += 3)
             {
-                int randIx = Random.Range(0, houses.Length);
-                GameObject house = houses[randIx];
-                //Instantiate(house, new Vector3(x, 0, z), Quaternion.identity);
+                int randIx = Random.Range(0, housePrefabs.Length);
+                GameObject house = housePrefabs[randIx];
+                Instantiate(house, new Vector3(x, 0, z), Quaternion.identity);
             }
         }
     }
