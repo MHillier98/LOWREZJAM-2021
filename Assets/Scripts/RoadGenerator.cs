6d6cc80d6b802959777d04d8010df78c8681b305
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-08-04 00:44:34 +1000

    Created all house prefabs + Updated road generation system with better houses

---
+++ Assets/Scripts/RoadGenerator.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGenerator : MonoBehaviour
+{
+    public GameObject roadObject;
+    public List<Vector3> roadLocations;
+
+    public GameObject[] houseObjects;
+    public List<Vector3> houseLocations;
+
+    private void Start()
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            RandomWalk(0, 0, 250);
+        }
+    }
+
+    private void RandomWalk(float globalX, float globalZ, int counter)
+    {
+        if (counter > 0)
+        {
+            counter--;
+
+            int length = 4;
+            int randLength = Random.Range(1, 5);
+            length *= randLength;
+
+            SpawnRoad(globalX, globalZ);
+
+            int randDirection = Random.Range(0, 4);
+            if (globalZ > 90 && randDirection == 0) { randDirection = 1; }
+            if (globalZ < -90 && randDirection == 1) { randDirection = 0; }
+            if (globalX > 90 && randDirection == 2) { randDirection = 3; }
+            if (globalX < -90 && randDirection == 3) { randDirection = 2; }
+
+            switch (randDirection)
+            {
+                case 0:
+                    {
+                        for (int z = 0; z < length; z++)
+                        {
+                            SpawnRoad(globalX, globalZ + z);
+                        }
+                        RandomWalk(globalX, globalZ + length, counter);
+                        break;
+                    }
+                case 1:
+                    {
+                        for (int z = -length; z < 0; z++)
+                        {
+                            SpawnRoad(globalX, globalZ + z);
+                        }
+                        RandomWalk(globalX, globalZ - length, counter);
+                        break;
+                    }
+
+                case 2:
+                    {
+                        for (int x = 0; x < length; x++)
+                        {
+                            SpawnRoad(globalX + x, globalZ);
+                        }
+                        RandomWalk(globalX + length, globalZ, counter);
+                        break;
+                    }
+
+                case 3:
+                    {
+                        for (int x = -length; x < 0; x++)
+                        {
+                            SpawnRoad(globalX + x, globalZ);
+                        }
+                        RandomWalk(globalX - length, globalZ, counter);
+                        break;
+                    }
+            }
+
+        }
+    }
+
+    private void SpawnRoad(float globalX, float globalZ)
+    {
+        Vector3 roadLoc = new Vector3(globalX, 0, globalZ);
+        if (!roadLocations.Contains(roadLoc))
+        {
+            Instantiate(roadObject, roadLoc, Quaternion.identity);
+            roadLocations.Add(roadLoc);
+
+            SpawnHouse(globalX, globalZ + 1.5f, 0f);
+            SpawnHouse(globalX, globalZ - 1.5f, 180f);
+            SpawnHouse(globalX + 1.5f, globalZ, 90f);
+            SpawnHouse(globalX - 1.5f, globalZ, 270f);
+        }
+    }
+
+    private void SpawnHouse(float globalX, float globalZ, float rotation)
+    {
+        Vector3 houseLoc = new Vector3(globalX, 0, globalZ);
+        if (!houseLocations.Contains(houseLoc))
+        {
+            if (HouseHasSpace(houseLoc))
+            {
+                int randHouse = Random.Range(0, houseObjects.Length);
+                GameObject house = houseObjects[randHouse];
+                Instantiate(house, houseLoc, Quaternion.Euler(0, rotation, 0));
+                houseLocations.Add(houseLoc);
+            }
+        }
+    }
+
+    private bool HouseHasSpace(Vector3 newPos)
+    {
+        foreach (Vector3 house in houseLocations)
+        {
+            if (Vector3.Distance(house, newPos) < 1.5f)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
